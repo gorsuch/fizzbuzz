08076c3b70acdfa45da5d5d60a32d0a9526b9f7a
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-01-04 20:10:48 -0500

    pending spec definitions

--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -11,4 +11,9 @@ describe FizzBuzz, "#calculate" do
     fb = FizzBuzz.new
     fb.calculate(15).should == "FizzBuzz"
   end
+  
+  it "returns Fizz for all multiples of 3"
+  it "returns Buzz for all multiples of 5"
+  it "returns the passed number if not a multiple of 3 or 5"
+  
 end
