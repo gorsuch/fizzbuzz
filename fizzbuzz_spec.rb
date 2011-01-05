611541745804fcd5b051c75a8ab85c4401649640
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-01-04 20:12:09 -0500

    completed spec with 3 failures

--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -11,12 +11,17 @@ describe FizzBuzz, "#calculate" do
   let(:fb) { FizzBuzz.new }
   
   it "returns FizzBuzz for all multiples of 3 and 5" do
-    fb = FizzBuzz.new
     fb.calculate(15).should == "FizzBuzz"
   end
   
-  it "returns Fizz for all multiples of 3"
-  it "returns Buzz for all multiples of 5"
-  it "returns the passed number if not a multiple of 3 or 5"
+  it "returns Fizz for all multiples of 3" do
+    fb.calculate(3).should == "Fizz"
+  end
+  it "returns Buzz for all multiples of 5" do
+    fb.calculate(5).should == "Buzz"
+  end
+  it "returns the passed number if not a multiple of 3 or 5" do
+    fb.calculate(77).should == 77
+  end
   
 end
